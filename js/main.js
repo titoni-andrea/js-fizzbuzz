eb57ba9bc09198f031ed51d8f462e435443e9d89
titoni-andrea <andreatitoni@gmail.com>
2024-04-19 17:02:23 +0200

    compleatato anche in pagina

--- js/main.js
+++ js/main.js
@@ -1,14 +1,44 @@
+const contFlex = document.getElementById("cont-flex")
+
 for (i = 1; i <= 100; i++) {
     if (((i % 3) == 0) && ((i % 5) == 0)) {
         console.log("fizzbuzz")
+        const fizzBuzz = document.createElement("div")
+        fizzBuzz.append(i)
+        fizzBuzz.classList.add("square")
+        fizzBuzz.classList.add("fizzbuzz")
+        fizzBuzz.innerHTML = "fizzbuzz"
+
+        contFlex.append(fizzBuzz)
+        
     }
     else if ((i % 5) == 0) {
         console.log("buzz")
+        const fizzBuzz = document.createElement("div")
+        fizzBuzz.append(i)
+        fizzBuzz.classList.add("square")
+        fizzBuzz.classList.add("buzz")
+        fizzBuzz.innerHTML = "buzz"
+
+        contFlex.append(fizzBuzz)
     }
     else if ((i % 3) == 0) {
         console.log("fizz")
+        const fizzBuzz = document.createElement("div")
+        fizzBuzz.append(i)
+        fizzBuzz.classList.add("square")
+        fizzBuzz.classList.add("fizz")
+        fizzBuzz.innerHTML = "fizz"
+
+        contFlex.append(fizzBuzz)
     }
     else {
         console.log(i)
+        const fizzBuzz = document.createElement("div")
+        fizzBuzz.append(i)
+        fizzBuzz.classList.add("square")
+        fizzBuzz.innerHTML = i
+
+        contFlex.append(fizzBuzz)
     }
 }
