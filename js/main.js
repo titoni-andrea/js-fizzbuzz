7dcc61e8c29aae0869f9878f98d86bb4cb6ba337
titoni-andrea <andreatitoni@gmail.com>
2024-04-19 14:21:51 +0200

    stampato correttamente in console.log

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 for (i = 1; i <= 100; i++) {
-    console.log(i)
+    if (((i % 3) == 0) && ((i % 5) == 0)) {
+        console.log("fizzbuzz")
+    }
+    else if ((i % 5) == 0) {
+        console.log("buzz")
+    }
+    else if ((i % 3) == 0) {
+        console.log("fizz")
+    }
+    else {
+        console.log(i)
+    }
 }
